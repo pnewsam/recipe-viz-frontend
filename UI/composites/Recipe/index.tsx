9bbda2b42eba18fc48907f80ec6f5527efadcf6e
Paul NEWSAM <paulnewsam@Pauls-MBP.fios-router.home>
2021-04-20 21:39:55 -0400

    Adds React Query and first query.

--- UI/composites/Recipe/index.tsx
+++ UI/composites/Recipe/index.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import OverviewSection from "../OverviewSection";
 import IngredientsSection from "../IngredientsSection";
 import ToolsSection from "../ToolsSection";
+import { useRecipes } from "../../../queries";
 import { mockRecipe } from "../../../models";
 
 export const Recipe = () => {
+  const { data: recipes } = useRecipes();
   const recipe = mockRecipe;
+
   return (
     <div>
       <h1>Recipe</h1>
+      {JSON.stringify(recipes)}
       <OverviewSection recipe={recipe}></OverviewSection>
       <IngredientsSection recipe={recipe}></IngredientsSection>
       <ToolsSection></ToolsSection>
